c05026cc0079d622e7999e3bf50b69ce2339bf34
zhangying05 <zhangying3210@gmail.com>
2013-12-24 11:01:22 +0800

    [feature]提供数据库同步,setting界面支持

--- static/js/a.js
+++ static/js/a.js
@@ -1,30 +1,26 @@
 
 $(document).ready(function(){
     $('#syncBtn').click(function(event){
-        var url = '/repos/sync?lastSyncTime=null&currentSyncTime=' + Date.parse(new Date()),
+        var url = '/repos/sync?repos=localhost:3459&lastSyncTime=null&currentSyncTime=' + Date.parse(new Date()),
             options = {
                 type: "GET",
                 url: url,
-//                data: {
-//                    name : nameval,
-//                    auth : auth
-//                },
                 async: false,
                 error: function(request){
 //                    hint(request.responseText);
-                    console.log(error);
+                    console.log("err" + request);
                 },
                 success: function(data){
 //                    hint(data.msg + "\r\n redirect to your page now");
 //                    setTimeout(function(){
 //                        location.href = data.url;
 //                    }, 2000);
-                    console.log(CharacterData);
+                    console.log("succ" + data);
                 }
             };
 console.log(new Date());
         console.log(url);
-//        $.ajax(options);
+        $.ajax(options);
         event.preventDefault();
 
     });
